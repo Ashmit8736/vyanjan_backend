0f0317e279ac7a27e27a0fbab1a953bee9a49d0d
Ansh Agarwal <anshagarwalindal2002@gmail.com>
2026-01-21 17:48:06 +0530

    branch ka user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, getUsers, getUserBranchStats } from "../controllers/userController.js";
+import { registerUser, getUsers, getUserBranchStats, addBranchUser, getBranchUsers } from "../controllers/userController.js";
 import superAdminAuth, { userAuth1 } from "../middlewares/authMiddlewares.js";
 import {addBranch, getMyBranches } from "../controllers/branchController.js";
 import userAuth from "../middlewares/userMiddleware.js";
@@ -12,5 +12,10 @@ router.get("/branch-stats", userAuth1, getUserBranchStats);
 
 router.post("/create", userAuth , addBranch );
 router.get("/branches", userAuth , getMyBranches);
+
+
+router.post("/branchUser", userAuth, addBranchUser);
+
+router.get("/branch/:branchId", userAuth, getBranchUsers);
 
 export default router;
