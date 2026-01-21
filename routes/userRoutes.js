e9c525feb5984dd4b1404985299808d7901530b2
emojija <emojija@gmail.com>
2026-01-21 12:57:04 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerUser, getUsers } from "../controllers/userController.js";
-import superAdminAuth from "../middlewares/authMiddlewares.js";
+import { registerUser, getUsers, getUserBranchStats } from "../controllers/userController.js";
+import superAdminAuth, { userAuth } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
 router.post("/signup", superAdminAuth, registerUser);
 router.get("/list", superAdminAuth, getUsers);
+router.get("/branch-stats", userAuth, getUserBranchStats);
 
 export default router;
